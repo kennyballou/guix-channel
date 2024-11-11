dca337e67a313e852f36e1887f927d1644c17166
Kenny Ballou <kb@devnulllabs.io>
2024-11-10 19:26:37 -0800

    jdt-language-server: update to version 1.41.0
    Signed-off-by: Kenny Ballou <kb@devnulllabs.io>

--- kbg/packages/jdtls.scm
+++ kbg/packages/jdtls.scm
@@ -14,8 +14,8 @@
 
 (define-public jdtls
   (let* ((baseurl "https://download.eclipse.org/jdtls/milestones")
-         (version-base "1.36.0")
-         (version-date "202405301306")
+         (version-base "1.41.0")
+         (version-date "202410311350")
          (version (string-append version-base "-" version-date))
          (name "jdt-language-server"))
     (package
@@ -27,7 +27,7 @@
               (file-name (string-append name "-" version ".tar.gz"))
               (sha256
                (base32
-                "0yxf7nq7y6v5cya5vl662dbgda0lyjgghmpx9ynir9pl0r6jg3h2"))))
+                "1xcpm31a64ldszf80g9cif0wajry6lrfk0a3bydkgg6mj64pxaxd"))))
      (inputs (list python-wrapper))
      (build-system copy-build-system)
      (arguments
